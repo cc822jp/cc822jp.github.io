69428fa85c73d81bf66c9608e876b433ccccd69f
Tatsuya Sasaki <sasaki@crystalcode.jp>
2020-01-12 20:21:16 +0900

    Updates

---
+++ precache-manifest.29aaed8280d01e544fc82890b4bff66e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dae01a8a0875b415bfae6204a8cf920",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b37092ef90cce426082a",
+    "url": "/static/css/main.dd0ee575.chunk.css"
+  },
+  {
+    "revision": "c7bab1b4031440fa92a1",
+    "url": "/static/js/2.ac04f6ca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ac04f6ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b37092ef90cce426082a",
+    "url": "/static/js/main.b02ef91b.chunk.js"
+  },
+  {
+    "revision": "a65face50cecd87ddcc7",
+    "url": "/static/js/runtime-main.a02af5e7.js"
+  },
+  {
+    "revision": "0e4fdb50878d2d3709ef3c186422ae93",
+    "url": "/static/media/monte-carlo.0e4fdb50.jpg"
+  }
+]);
